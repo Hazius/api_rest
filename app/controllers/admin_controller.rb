4bd3201259dd5b068fa0e617c09fc79d6a96a01b
pavel <pavel@ubuntu.(none)>
2014-07-31 09:57:59 +0400

    commit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,54 +1,54 @@
 class AdminController < ApplicationController
 
-	before_action :admin_user
+  before_action :admin_user
 
-	def index
-  		
-	end
+  def index
+      
+  end
 
-	def edit_file
-		@content = ""
-		@file_path = ""
-	end
+  def edit_file
+    @content = ""
+    @file_path = ""
+  end
 
-	def show_file
+  def show_file
 
-		@file_path = params[:file_path]
-  		
-		if (File.exist?(@file_path))
-			file = File.open(@file_path)
-			@file_content = file.read()
-			file.close()
-			flash.now[:notice] = "Файл открыт"
-		else
-			file = File.new(@file_path, "w")
-			@file_content = ""
-			flash.now[:notice] = "Файл создан"
-		end
+    @file_path = params[:file_path]
+      
+    if (File.exist?(@file_path))
+      file = File.open(@file_path)
+      @file_content = file.read()
+      file.close()
+      flash.now[:notice] = "Файл открыт"
+    else
+      file = File.new(@file_path, "w")
+      @file_content = ""
+      flash.now[:notice] = "Файл создан"
+    end
 
-		render "edit_file"
+    render "edit_file"
 
-	end
+  end
 
-	def save_file
-		@file_path = params[:file_path]
-		@file_content = params[:file_content]
+  def save_file
+    @file_path = params[:file_path]
+    @file_content = params[:file_content]
 
-		file = File.open(@file_path, "w")  
-		file.write(@file_content)  		
-		file.close()
+    file = File.open(@file_path, "w")  
+    file.write(@file_content)     
+    file.close()
 
-		flash.now[:notice] = "Данные сохранены"
+    flash.now[:notice] = "Данные сохранены"
 
-		render "edit_file"
+    render "edit_file"
     
-	end
+  end
 
-	private
+  private
 
-		def admin_user	
-			#redirect_to(root_url) unless signed_in?
-			#redirect_to(root_url) unless current_user().admin?
-		end
+    def admin_user  
+      #redirect_to(root_url) unless signed_in?
+      #redirect_to(root_url) unless current_user().admin?
+    end
 
 end
